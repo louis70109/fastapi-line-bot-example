3a3eef0f205bcd522025d0889b60c8f083ab65e5
NiJia Lin <louis70109@gmail.com>
2021-04-21 01:32:26 +0800

    add 200 test case

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,3 +1,4 @@
+import os
 import unittest
 from _pytest.monkeypatch import MonkeyPatch
 from mock import patch
@@ -15,10 +16,26 @@ def test_read_main():
 
 
 def test_read_bot():
-    response = client.post("/webhooks/line", data=b'{"events":[],"destination":"U000000000000000000000003d9"}',
-                           headers={"Content-Type": "multipart/form-data", 'X-Line-Signature': '123123'})
-    assert response.status_code == 400
-    assert response.json() == {'detail': 'chatbot handle body error.'}
+    import base64
+    import hashlib
+    import hmac
+
+    channel_secret = os.getenv('LINE_CHANNEL_SECRET')
+    body = '{"events":[],"destination":"U000000000000000000000003d9"}'
+    hash = hmac.new(channel_secret.encode('utf-8'),
+                    body.encode('utf-8'), hashlib.sha256).digest()
+    signature = base64.b64encode(hash)
+    response = client.post(
+        url="/webhooks/line",
+        data=body,
+        headers={
+            "Content-Type": "multipart/form-data",
+            'X-Line-Signature': signature.decode('UTF-8')
+        })
+
+    assert response.url == 'http://testserver/webhooks/line'
+    assert response.status_code == 200
+    assert response.json() == 'OK'
 
 # class TestClient(unittest.TestCase):
 #     def setUp(self):
